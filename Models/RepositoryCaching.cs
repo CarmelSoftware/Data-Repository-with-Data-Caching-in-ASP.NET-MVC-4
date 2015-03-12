73139352aa2ee0a194cf133671aa2e8638e5c9f5
Carmel Shvartzman <carmel.aspnet@gmail.com>
2015-03-12 21:27:13 +0200

    Update RepositoryCaching.cs

--- Models/RepositoryCaching.cs
+++ Models/RepositoryCaching.cs
@@ -1,1 +1,38 @@
 //  TODO: create Caching Class to store data in MemoryCache :
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Caching;
+
+namespace RepositoryWithCaching.Models
+{
+    public class RepositoryCaching
+    {
+        public ObjectCache Cache
+        {
+            get { return MemoryCache.Default; }
+        }
+
+        public bool IsInMemory(string Key)
+        {
+            return Cache.Contains(Key);
+        }
+
+        public void Add(string Key, object Value, int Expiration)
+        {
+            Cache.Add(Key, Value, new CacheItemPolicy().AbsoluteExpiration = DateTime.Now + TimeSpan.FromMinutes(Expiration));
+        }
+
+        public IQueryable<T> FetchData<T>(string Key) where T : class
+        {
+            return Cache[Key] as IQueryable<T>;
+        }
+
+        public void Remove(string Key)
+        {
+            Cache.Remove(Key);
+        }
+
+    }
+}
